ba28913f33c53482ed00014a1d21741b7a79245d
shufschmid <shufschmid@gmail.com>
2022-10-27 09:51:05 +0200

    erster test

--- site/index.tsx
+++ site/index.tsx
@@ -1,57 +1,66 @@
-import ReactDOM from 'react-dom'
-import React, { useCallback, useEffect, useRef } from 'react'
+import ReactDOM from "react-dom";
+import React, { useCallback, useEffect, useRef } from "react";
 
-import JSConfetti from '../src/index'
-import { generateRandomArrayElement  } from '../src/generateRandomArrayElement'
-import { IAddConfettiConfig } from '../src/types'
-
+import JSConfetti from "../src/index";
+import { generateRandomArrayElement } from "../src/generateRandomArrayElement";
+import { IAddConfettiConfig } from "../src/types";
 
 const CONFETTI_ARGS: IAddConfettiConfig[] = [
   {},
   { confettiRadius: 12, confettiNumber: 100 },
-  { emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'] },
-  { emojis: ['⚡️', '💥', '✨', '💫'] },
-  { emojis: ['🦄'], confettiRadius: 100, confettiNumber: 30 },
+  { emojis: ["🌈", "⚡️", "💥", "✨", "💫", "🌸"] },
+  { emojis: ["⚡️", "💥", "✨", "💫"] },
+  { emojis: ["🦄"], confettiRadius: 100, confettiNumber: 30 },
   {
-    confettiColors: ['#ffbe0b', '#fb5607', '#ff006e', '#8338ec', '#3a86ff'],
+    confettiColors: ["#ffbe0b", "#fb5607", "#ff006e", "#8338ec", "#3a86ff"],
     confettiRadius: 10,
     confettiNumber: 150,
   },
   {
-    confettiColors: ['#9b5de5', '#f15bb5', '#fee440', '#00bbf9', '#00f5d4'],
+    confettiColors: ["#9b5de5", "#f15bb5", "#fee440", "#00bbf9", "#00f5d4"],
     confettiRadius: 6,
     confettiNumber: 300,
   },
-]
+];
 
 function App(): JSX.Element {
-  const jsConfettiRef = useRef<JSConfetti>()
+  const jsConfettiRef = useRef<JSConfetti>();
 
   useEffect(() => {
-    jsConfettiRef.current = new JSConfetti()
+    jsConfettiRef.current = new JSConfetti();
 
     const timeoutId = setTimeout(() => {
       if (jsConfettiRef.current) {
-        jsConfettiRef.current.addConfetti(generateRandomArrayElement(CONFETTI_ARGS)).then(() => console.log("Initial batch completed"))
+        jsConfettiRef.current
+          .addConfetti(generateRandomArrayElement(CONFETTI_ARGS))
+          .then(() => console.log("Initial batch completed"));
       }
-    }, 1000)
+    }, 1000);
 
-    return () => clearTimeout(timeoutId)
-  }, [])
+    return () => clearTimeout(timeoutId);
+  }, []);
 
   const onButtonClick = useCallback(() => {
+    let items =["super", "wow", "absoluter hammer"]
+
+    document.getElementById("myButton2").innerHTML =
+      items[Math.floor(Math.random() * items.length)]
+
     if (jsConfettiRef.current) {
-      jsConfettiRef.current.addConfetti(generateRandomArrayElement(CONFETTI_ARGS)).then(() => console.log("Manual batch completed"))
+      jsConfettiRef.current
+        .addConfetti(generateRandomArrayElement(CONFETTI_ARGS))
+        .then(() => console.log("Manual batch completed"));
     }
-  }, [jsConfettiRef])
+  }, [jsConfettiRef]);
 
   return (
     <>
-      <button className="button" onClick={onButtonClick}>Click me!</button>
+      <button className="button" onClick={onButtonClick} id="myButton2">
+        gute Nachricht!
+      </button>
     </>
-  )
+  );
 }
 
-
-const appContainer = document.getElementById('app')
-ReactDOM.render(<App />, appContainer)
+const appContainer = document.getElementById("app");
+ReactDOM.render(<App />, appContainer);
